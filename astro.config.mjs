ca4816d2c0af23abec8d59b238691df81bbbbdfb
Dylan Baker <dylanbaker7@gmail.com>
2023-10-12 12:41:40 -0400

    g

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-import nodejs from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://symplifysolutions.com',
   output: "server",
-  adapter: nodejs({mode: "standalone"}),
-  
-  integrations: [mdx(), sitemap(), react()],
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [mdx(), sitemap(), react()]
 });
