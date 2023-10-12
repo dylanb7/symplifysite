ff7284816b0a73b1d3a2589d9c2b08508c16c988
Dylan Baker <dylanbaker7@gmail.com>
2023-10-12 12:09:23 -0400

    h

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
+import nodejs from '@astrojs/node';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://symplifysolutions.com',
   output: "server",
+  adapter: nodejs(),
   integrations: [mdx(), sitemap(), react()],
 });
