eb5555e52de6966954aa13c36b7a944ad2396ebb
Dylan Baker <dylanbaker7@gmail.com>
2024-04-29 15:52:29 -0400

    changed base path

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  distDir: 'dist',
+  basePath: 'dist',
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
