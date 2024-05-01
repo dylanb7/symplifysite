e4ef10797f3258bfc3cee0a89c0ebef4f4813f75
Dylan Baker <dylanbaker7@gmail.com>
2024-05-01 13:43:28 -0400

    h

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ await import("./src/env.js");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
